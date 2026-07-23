c02ead4a30ce4924ffefd8c0399ed0ebccc230bd
GUSTAVO QUEIROGA <gutomq@hotmail.com>
2026-07-23 20:41:08 -0300

    Update Caixa eletrônico simples (menu).py

--- Caixa eletrônico simples (menu).py
+++ Caixa eletrônico simples (menu).py
@@ -32,3 +32,47 @@ def menu():
         else:
             print("Opção inválida!")
     
+    def ver_saldo():
+        print(f"\nSeu saldo é R$ {saldo:.2f}")
+    
+    def depositar():
+        global saldo
+        global extrato
+
+    valor = float(input("Valor do depósito: R$ "))
+
+    if valor > 0:
+        saldo += valor
+        extrato += f"Depósito: +R$ {valor:.2f}\n"
+        print("Depósito realizado com sucesso!")
+    else:
+        print("Valor inválido.")
+        
+    def sacar():
+        global saldo
+        global extrato
+
+    valor = float(input("Valor do saque: R$ "))
+
+    if valor <= 0:
+        print("Valor inválido.")
+
+    elif valor > saldo:
+        print("Saldo insuficiente.")
+
+    else:
+        saldo -= valor
+        extrato += f"Saque: -R$ {valor:.2f}\n"
+        print("Saque realizado com sucesso!")
+    
+    def ver_extrato():
+            print("\n========== EXTRATO ==========")
+
+    if extrato == "":
+        print("Nenhuma movimentação.")
+
+    else:
+        print(extrato)
+
+        print(f"Saldo atual: R$ {saldo:.2f}")
+        

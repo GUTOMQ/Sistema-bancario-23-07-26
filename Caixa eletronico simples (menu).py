38c2d7c9cf0312f4116a0dee1f84a8b1602b2dd3
GUSTAVO QUEIROGA <gutomq@hotmail.com>
2026-07-23 21:53:17 -0300

    Create Caixa eletronico simples (menu).py

---
+++ Caixa eletronico simples (menu).py
@@ -0,0 +1,42 @@
+#Sistema de caixa eletronico simples em Python
+saldo = 100
+
+def exibir_banco(): 
+    print("\n===== CAIXA ELETRONICO =====")
+    print("1- Consultar Saldo")
+    print("2- Depositar Dinheiro")
+    print("3- Sacar Dinheiro")  
+    print("4- Ver Extrato")
+    print("5- Sair")
+    
+def consultar_saldo():    
+    print(f"\nSeu saldo atual é: R$ {saldo}")
+    pass   
+def depositar_dinheiro():
+    global saldo
+    valor = float(input("\nDigite o valor a ser depositado R$ "))
+    if valor > 0:
+        saldo += valor
+        print(f"\nDeposito de R$ {valor} realizado com sucesso."    )
+    pass
+def sacar_dinheiro():
+    global saldo
+    valor = float(input("\nDigite o valor a ser sacado R$ "))
+    if valor > 0 and valor <= saldo:
+        saldo -= valor
+        print(f"\nSaque de R$ {valor} realizado com sucesso.")
+    else:
+        print("\nSaldo insuficiente ou valor inválido.")
+    pass
+
+def main():
+    while True:
+       exibir_banco()
+    opcao = input("Escolha uma opção: ")
+    if opcao == "1":
+        consultar_saldo()
+    elif opcao == "2":
+        depositar_dinheiro()
+    elif opcao == "3":  
+        sacar_dinheiro()
+        
